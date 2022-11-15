6907efce3fa37c7c081c0abbdaa0bb6718a01616
lkw <1234qwer>
2022-11-15 18:10:07 +0900

    ~7.1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import { useEffect, useState } from "react";
-
-function Hello() {
-    useEffect(() => {
-        console.log("hi :)");
-        return () => console.log("bye :(");
-    }, []);
-    return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-    const [showing, setShowing] = useState(false);
-    const onClick = () => setShowing((prev) => !prev);
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (e) => setToDo(e.target.value);
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDo("");
+        setToDos((currentArray) => [toDo, ...currentArray]);
+    };
+    console.log(toDos);
     return (
-        <div>
-            {showing ? <Hello /> : null}
-            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+        <div className="container mt-3">
+            <h2 className="mb-4">My To Dos ({toDos.length})</h2>
+            <form className="d-flex gap-2" onSubmit={onSubmit}>
+                <input
+                    className="form-control w-auto"
+                    onChange={onChange}
+                    value={toDo}
+                    type="text"
+                    placeholder="Write your to do..."
+                />
+                <button className="btn btn-primary">Add To Do</button>
+            </form>
+            <hr />
+            <ul className="list-group">
+                {toDos.map((item, index) => (
+                    <li key={index} className="list-group-item">
+                        {item}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
