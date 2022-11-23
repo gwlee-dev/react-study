0045601a914959db16817c616ea25bc990f78f7e
lkw <1234qwer>
2022-11-23 18:00:40 +0900

    ~ 7.3

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 function App() {
-    const [toDo, setToDo] = useState("");
-    const [toDos, setToDos] = useState([]);
-    const onChange = (e) => setToDo(e.target.value);
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (toDo === "") {
-            return;
-        }
-        setToDo("");
-        setToDos((currentArray) => [toDo, ...currentArray]);
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const json = await (
+            await fetch(
+                `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+            )
+        ).json();
+        setMovies(json.data.movies);
+        setLoading(false);
+        console.log(movies);
     };
-    console.log(toDos);
+
+    useEffect(() => getMovies, []);
     return (
         <div className="container mt-3">
-            <h2 className="mb-4">My To Dos ({toDos.length})</h2>
-            <form className="d-flex gap-2" onSubmit={onSubmit}>
-                <input
-                    className="form-control w-auto"
-                    onChange={onChange}
-                    value={toDo}
-                    type="text"
-                    placeholder="Write your to do..."
-                />
-                <button className="btn btn-primary">Add To Do</button>
-            </form>
-            <hr />
-            <ul className="list-group">
-                {toDos.map((item, index) => (
-                    <li key={index} className="list-group-item">
-                        {item}
-                    </li>
-                ))}
-            </ul>
+            {loading ? (
+                <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            ) : (
+                <ul className="list-group">
+                    {movies.map((movie) => (
+                        <li key={movie.id} className="list-group-item">
+                            <div className="row align-items-center">
+                                <div className="col-2">
+                                    <img
+                                        src={movie.medium_cover_image}
+                                        alt="poster"
+                                        className="w-100"
+                                    ></img>
+                                </div>
+                                <div className="col-10">
+                                    <span className="fw-bold">
+                                        {movie.title}
+                                    </span>
+                                    {movie.genres.map((genre) => (
+                                        <span
+                                            key={genre}
+                                            className="badge bg-secondary ms-2"
+                                        >
+                                            {genre}
+                                        </span>
+                                    ))}
+                                    <p className="mb-0">{movie.summary}</p>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }
